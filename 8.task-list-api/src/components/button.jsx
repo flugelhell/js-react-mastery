bf2c26cccb925f8ad0c0432545f1d41e35f9d60c
flugelhell <bleadhelsink@gmail.com>
2024-01-11 17:02:50 +0700

    style auth page

--- 8.task-list-api/src/components/button.jsx
+++ 8.task-list-api/src/components/button.jsx
@@ -1,11 +1,11 @@
-export default function Button({ text, onClick, color = "blue" }) {
+export default function Button({ type = "button", text, onClick, color = "blue" }) {
   let style = "";
   if (color == "blue") style = "p-2 shadow rounded text-slate-100 bg-sky-500  hover:bg-sky-600 active:bg-sky-700";
   if (color == "green") style = "p-2 shadow rounded text-slate-100 bg-green-500  hover:bg-green-600 active:bg-green-700";
   if (color == "red") style = "p-2 shadow rounded text-slate-100 bg-red-500 hover:bg-red-600 active:bg-red-700";
 
   return (
-    <button type="button" className={style} onClick={onClick}>
+    <button type={type} className={style} onClick={onClick}>
       {text}
     </button>
   );
--- 8.task-list-api/src/pages/auth.jsx
+++ 8.task-list-api/src/pages/auth.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const Auth = () => {
-  const [login, setLogin] = useState();
+  const [isloginForm, setIsLoginForm] = useState();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -44,34 +44,38 @@ const Auth = () => {
   const onChangePassword = (e) => {
     setPassword(e.target.value);
   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    notify("info", "form submited");
+  };
 
   return (
     <div className="flex h-screen items-center justify-center">
       <ToastContainer newestOnTop />
       <div className="p-4 border-2 w-96">
-        <h1 className="text-center pb-2 text-xl font-bold">{login ? "LOGIN" : "REGISTER"}</h1>
-        <form className="flex flex-col">
-          {!login && <Input type="text" placeholder="Nama" value={name} onChange={(e) => onChangeName(e)} />}
-          <Input type="email" placeholder="Email" value={email} onChange={(e) => onChangeEmail(e)} />
+        <h1 className="text-center pb-2 text-xl font-bold">{isloginForm ? "LOGIN" : "REGISTER"}</h1>
+        <form className="flex flex-col" onSubmit={(e) => onSubmit(e)}>
+          {!isloginForm && <Input type="text" placeholder="Nama" value={name} onChange={(e) => onChangeName(e)} required />}
+          <Input type="email" placeholder="Email" value={email} onChange={(e) => onChangeEmail(e)} required />
           <div className="relative">
-            <Input type={isPasswordVisible ? "text" : "password"} placeholder="Password" value={password} onChange={(e) => onChangePassword(e)} className="pr-20" autoComplete="false" />
+            <Input type={isPasswordVisible ? "text" : "password"} placeholder="Password" value={password} onChange={(e) => onChangePassword(e)} className="pr-20" required />
             <i className={`bx ${isPasswordVisible ? "bx-low-vision" : "bx-show"} btn text-xl absolute right-0 bg-transparent border-0 shadow-none ring-0`} onClick={() => setIsPasswordVisible(!isPasswordVisible)}></i>
           </div>
           <div className="mx-auto m-2">
-            <Button text="Submit" color="blue" onClick={() => notify("success", name)} />
+            <Button type="submit" text="Submit" color="blue" />
           </div>
           <p className="mt-2 italic text-slate-500">
-            {login ? (
+            {isloginForm ? (
               <>
                 Belum punya akun? Silahkan&nbsp;
-                <span className="cursor-pointer text-blue-400" onClick={() => setLogin(!login)}>
+                <span className="cursor-pointer text-blue-400" onClick={() => setIsLoginForm(!isloginForm)}>
                   Register
                 </span>
               </>
             ) : (
               <>
                 Sudah punya akun? Silahkan&nbsp;
-                <span className="cursor-pointer text-blue-400" onClick={() => setLogin(!login)}>
+                <span className="cursor-pointer text-blue-400" onClick={() => setIsLoginForm(!isloginForm)}>
                   Login
                 </span>
               </>
